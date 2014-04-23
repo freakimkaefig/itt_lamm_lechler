e2f7e6049fe894abf86774fed7eb6e16ae19870a
freakimkaefig <lukas.lamm89@googlemail.com>
2014-04-23 23:13:37 +0200

    created class for reading setup
    fixed some pep8 errors

--- 02_assignment/space_counter.py
+++ 02_assignment/space_counter.py
@@ -4,16 +4,48 @@
 import sys
 from PyQt4 import QtGui, QtCore
 import logging
+import csv
+import random
+import itertools
 # import time
 
+
+class Setup():
+
+    widths = []
+    distances = []
+    repetitions = 4
+    combinations = []
+    counter = -1
+
+    def __init__(self):
+        self.read_setup_file()
+
+    def read_setup_file(self):
+        if len(sys.argv) > 1:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    temp = line.split()
+                    if temp[0] == "USER:":
+                        self.user = int(temp[1])
+
+                    if temp[0] == "WIDTHS:":
+                        for x in temp[1].split(','):
+                            self.widths.append(int(x))
+
+                    if temp[0] == "DISTANCES:":
+                        for x in temp[1].split(','):
+                            self.distances.append(int(x))
+
+
 class ClickRecorder(QtGui.QWidget):
-    
+
     def __init__(self):
         super(ClickRecorder, self).__init__()
         self.counter = 0
         self.initUI()
-        
-    def initUI(self):      
+
+    def initUI(self):
         self.text = "Please press 'space' repeatedly."
         self.setGeometry(300, 300, 280, 170)
         self.setWindowTitle('ClickRecorder')
@@ -29,23 +61,23 @@ class ClickRecorder(QtGui.QWidget):
         qp = QtGui.QPainter()
         qp.begin(self)
         self.drawText(event, qp)
-        self.drawRect(event, qp) 
+        self.drawRect(event, qp)
         qp.end()
-        
+
     def drawText(self, event, qp):
         qp.setPen(QtGui.QColor(168, 34, 3))
         qp.setFont(QtGui.QFont('Decorative', 32))
         if self.counter > 0:
-             self.text = str(self.counter)
-        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)        
-    
+            self.text = str(self.counter)
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)
+
     def drawRect(self, event, qp):
         if (self.counter % 2) == 0:
-             rect = QtCore.QRect(10,10, 30,30)
-             qp.setBrush(QtGui.QColor(34, 34, 200))
+            rect = QtCore.QRect(10, 10, 30, 30)
+            qp.setBrush(QtGui.QColor(34, 34, 200))
         else:
-             rect = QtCore.QRect(40,10, 30,30)
-             qp.setBrush(QtGui.QColor(200, 34, 34))
+            rect = QtCore.QRect(40, 10, 30, 30)
+            qp.setBrush(QtGui.QColor(200, 34, 34))
         qp.drawRoundRect(rect)
 
 
@@ -55,56 +87,45 @@ def initLogging():
     # file handler
     fh = logging.FileHandler('log.csv')
     fh.setLevel(logging.DEBUG)
-    # console handler    
+    # console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.ERROR)
     # set logging format
 
-    # asctime liefert zeitstempel folgend:  ‘2003-07-08 16:49:45,896’ [YYYY-MM-DD HH:MM:SS,MS]
-    # das Komma am ende vor MS bewirkt spaltenwechsel in csv-datei
+    #asctime liefert zeitstempel: [YYYY-MM-DD HH:MM:SS,MS]
+    #das Komma am ende vor MS bewirkt spaltenwechsel in csv-datei
 
     format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # add to handlers    
+    # add to handlers
     fh.setFormatter(format)
     ch.setFormatter(format)
-    # add handlers to log 
+    # add handlers to log
     log.addHandler(fh)
     log.addHandler(ch)
     # initial log
     log.info('Initial Log')
 
 
-def read_setup():
-    if len(sys.argv) > 1:
-        with open(sys.argv[1]) as file:
-            for line in file:
-                temp = line.split()
-                if temp[0] == "USER:":
-                    user = int(temp[1])
-                    print "User:" + str(user)
-                    
-                if temp[0] == "WIDTHS:":
-                    #print temp[1].split(',')
-                    widths = []
-                    for x in temp[1].split(','):
-                        widths.append(int(x))
-                    print "Widths:" + str(widths)
-                        
-                    
-                if temp[0] == "DISTANCES:":
-                    #print temp[1]
-                    distances = []
-                    for x in temp[1].split(','):
-                        distances.append(int(x))
-                    print "Distances:" + str(distances)
-                
-                
-            
-        
+def init_setup():
+    # creates new object of type setup
+    # reads setup from given file
+    setup = Setup()
+    print "User: " + str(setup.user)
+
+
+def log(user, width, distance):
+    logfile = open("user" + str(user) + ".csv", "w+")
+    d = {"user": user, "time(ms)": 327}
+    out = csv.DictWriter(logfile, ["width", "distance", "time(ms)"])
+    out.writeheader()
+    out.writerow(d)
+    logfile.close()
+
+
 def main():
     # prints secs since the epoch: print(time.time())
     initLogging()
-    read_setup()
+    init_setup()
     app = QtGui.QApplication(sys.argv)
     click = ClickRecorder()
     sys.exit(app.exec_())
