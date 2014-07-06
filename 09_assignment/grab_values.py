c6c34011150528d1e831f8802ff7a3da721ac865
led13714 <david.lechler@stud.uni-regensburg.de>
2014-07-06 11:04:59 +0000

    init 09 assignment

---
+++ 09_assignment/grab_values.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import wiimote
+import time
+import sys
+import csv
+
+wm = wiimote.connect(sys.argv[1])
+
+xp = yp = zp = 0
+iteration = 0
+logInit = 0        
+onceA = 0
+
+
+while True:
+    if wm.buttons["A"]:
+        onceA = 0
+        x,y,z = wm.accelerometer
+        if (x != xp) or (y != yp) or (z != zp):
+            print("%d,%d,%d") % (x,y,z)
+        xp,yp,zp = x,y,z
+        
+        logfile = open(str(iteration)+"data.csv", "a")
+        out = csv.DictWriter(logfile, ["Iteration", "X", "Y", "Z"])
+        if logInit == 0:
+            out.writeheader()
+            logInit = 1
+
+        d = {"Iteration": iteration, "X": xp, "Y": yp, "Z": zp}
+        out.writerow(d)
+        logfile.close()
+        
+        time.sleep(0.01)
+
+    if wm.buttons["B"]:
+        if(onceA == 0):
+            onceA = 1
+            print"new csv created"
+            iteration += 1
+            logInit = 0
+
+        
+wm.disconnect()
+time.sleep(1)
